7f62ed9a54efdc2ef22732294703a4a8272ef228
Anthony Suárez Cardona <anthonysuarez336@gmail.com>
2026-09-01 01:30:56 -0500

    Create Abstract GeometricFigure class

---
+++ GeometricFigure/Backend/GeometricFigure.cs
@@ -0,0 +1,19 @@
+﻿namespace Backend;
+
+public abstract class GeometricFigure
+{
+      
+//Properties
+    public string Name { get; set; } = null!;
+
+
+//Public Methods
+    
+    public abstract decimal GetArea();
+    public abstract decimal GetPerimeter();
+    public override string ToString()
+    {
+        return Name;
+    }
+}   
+
