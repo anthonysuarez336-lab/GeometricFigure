2ed411a3015b32399755df8bcc5859012f2ca742
Anthony Suárez Cardona <anthonysuarez336@gmail.com>
2026-09-06 20:35:20 -0500

    Add Trapeze Class And Adjust ToString Formatting

--- GeometricFigure/Backend/GeometricFigure.cs
+++ GeometricFigure/Backend/GeometricFigure.cs
@@ -18,7 +18,7 @@ public abstract class GeometricFigure
     public abstract double GetPerimeter();
     public override string ToString()
     {
-        return $"{Name,-15}=> Area.....:{GetArea(),15:N5}       Perimeter:{GetPerimeter(),15:N5}"; 
+        return $" {Name,-15}=> Area.....:{GetArea(),15:N5}       Perimeter:{GetPerimeter(),15:N5}"; 
     }
         
     }
